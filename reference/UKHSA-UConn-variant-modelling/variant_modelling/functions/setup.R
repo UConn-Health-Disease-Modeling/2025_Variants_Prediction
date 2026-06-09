3b18a7402190d4355fea9757384c2cda8768ea63
Youngji1435 <youngji1435@gmail.com>
2026-06-09 09:00:23 -0400

    Remove YJ and reference folders from tracking (kept locally)

--- reference/UKHSA-UConn-variant-modelling/variant_modelling/functions/setup.R
+++
@@ -1,9 +0,0 @@
-
-install.packages('tidyverse')
-install.packages('rjson')
-install.packages('mgcv')
-install.packages('gratia')
-install.packages('patchwork')
-install.packages('glue')
-install.packages('scales')
-install.packages('lubridate')
